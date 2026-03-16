d5744b795bea212234fe82786d26767647bb736c
Ronald <ronaldmachado.work@gmail.com>
2026-03-16 21:48:29 +0200

    add util to decode base64 URL encoded video

--- api/Util.php
+++ api/Util.php
@@ -15,4 +15,20 @@ class Util
         }, '');
     }
 
+    /**
+     * Decode base64 video to array with mime and data
+     * @param string $base64
+     * @return array
+     */
+    public function decodeBase64Video(string $base64): array {
+        $parts = explode(",", $base64);
+        $mime = $parts[0];
+        $data = $parts[1];
+
+        return [
+            'mime' => $mime,
+            'data' => base64_decode($data)
+        ];
+    }
+
 }
